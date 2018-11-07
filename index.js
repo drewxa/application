3140806324d118c3d1a2386678ad1502ffe510e3
drewxa <bo.ro.da@mail.ru>
2018-11-07 16:08:04 +0300

    change to create event

--- index.js
+++ index.js
@@ -6,20 +6,21 @@ module.exports = app => {
   app.log('Yay, the app was loaded!')
 
   // Add Branch Protection
-  app.on('repository_import.success', (context) => {
-    app.log('added protected branch to repo')
-    for (repo of context.payload.repositories_added) {
-      app.log(repo)
-      let full_name = repo.full_name
-      context.github.repos.updateBranchProtection(context.repo({
-        branch: 'master',
-        required_status_checks: null,
-        enforce_admins: true,
-        required_pull_request_reviews: {
-          require_code_owner_reviews: true,
-        },
-        restrictions: null
-      }))
+  app.on('create', (context) => {
+    app.log.info('on create event')
+    if (context.payload.ref_type === 'branch') {
+      if (context.payload.ref === context.payload.master_branch) {
+        app.log('added protected branch to repo')
+        context.github.repos.updateBranchProtection(context.repo({
+          branch: context.payload.master_branch,
+          required_status_checks: null,
+          enforce_admins: true,
+          required_pull_request_reviews: {
+            require_code_owner_reviews: true,
+          },
+          restrictions: null
+        }))
+      }
     }
   })
 }
