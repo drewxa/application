5faf16664dcf528c9cd05fe8f49f563eb9783b99
drewxa <bo.ro.da@mail.ru>
2018-11-09 01:16:17 +0300

    added coverage section

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
  * @param {import('probot').Application} app - Probot's Application class.
  */
 module.exports = app => {
+  const route = app.route('/')
+  var bodyParser = require('body-parser')
+  route.use(bodyParser.json())
+  route.use(bodyParser.urlencoded({
+    extended: false
+  }))
+
   app.log('Yay, the app was loaded!')
 
   // Add Branch Protection
@@ -11,6 +18,7 @@ module.exports = app => {
     if (context.payload.ref_type === 'branch') {
       if (context.payload.ref === context.payload.master_branch) {
         app.log('added protected branch to repo')
+        app.log.info(context.repo())
         context.github.repos.updateBranchProtection(context.repo({
           branch: context.payload.master_branch,
           required_status_checks: null,
@@ -22,5 +30,48 @@ module.exports = app => {
         }))
       }
     }
+  })
+
+  route.post('/coverage', async (req, res) => {
+    app.log.info('on coverage report')
+    app.log(req.body)
+    const report = Buffer.from(req.body.report, 'base64').toString()
+    const {pull_request, head_branch, head_sha, need_comment} = req.body
+    const {login, repo} = req.body.slug.split('/')
+  
+    const id = await foundInstallationId(app, login)
+    if (id === -1) {
+      res.sendStatus(404)
+      return
+    }
+  
+    const github = await app.auth(id)
+  
+    if (need_comment) {
+      const comment = {
+        number: pull_request,
+        repo: repo,
+        owner: login,
+        body: formatMessage(report)
+      }
+      app.log(comment)
+      github.issues.createComment(comment)
+    }
+  
+    context.github.checks.create({
+      owner: login,
+      repo: repo,
+      name: checkName(),
+      head_branch: head_branch,
+      head_sha: head_sha,
+      status: 'completed',
+      conclusion: 'success',
+      completed_at: new Date(),
+      output: {
+        title: 'Coverage report',
+        summary: formatMessage(report)
+      }
+    })
+    res.send('ok')
   })
 }
